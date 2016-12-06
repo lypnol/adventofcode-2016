104af1ea32bd458276c9bb8548304aa6b2fbe4fd
Ayoub Sbai <ayoub.sbai@datadoghq.com>
2016-12-06 14:06:26 -0500

    Added day 6

---
+++ day-6/part-2/ayoub.py
@@ -0,0 +1,19 @@
+from submission import Submission
+
+class AyoubSubmission(Submission):
+
+    def author(self):
+        return 'Ayoub'
+
+    def run(self, s):
+        s = s.rstrip()
+        n = 8
+        occ = [[ 0 for _ in range(26) ] for i in range(n)]
+
+        for line in s.split("\n"):
+            for i in range(n):
+                occ[i][ord(line[i]) - ord('a')] += 1
+
+        password = [ chr(ord('a') + min(enumerate(occ[i]), key=lambda x: x[1])[0]) for i in range(n) ]
+
+        return "".join(password)
