8beaf8ed1f024b4ab7ed3c807c546f60ae92fd80
ayoub <ayoub.sbai@student.ecp.fr>
2016-12-14 09:42:18 -0500

    Added day 14 part 1

---
+++ day-14/part-2/ayoub.py
@@ -0,0 +1,12 @@
+from submission import Submission
+
+
+class AyoubSubmission(Submission):
+
+    def author(self):
+        return 'Ayoub'
+
+    def run(self, s):
+        s = s.rstrip()
+        
+        return
