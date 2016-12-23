5dc898db6e45cf166fcc42b4269c3f6d1ab7339a
Ayoub Sbai <ayoub.sbai@datadoghq.com>
2016-12-23 11:12:52 -0500

    stash

--- day-22/part-1/ayoub.py
+++ day-22/part-1/ayoub.py
@@ -9,26 +9,19 @@ class AyoubSubmission(Submission):
     def run(self, s):
         s = s.rstrip().split('\n')[2:]
 
-        n = 31
-
         nodes = []
-        i = -1
-        j = 0
         for line in s:
             tokens = line.split()
-            if j == 0:
-                nodes.append([None for _ in range(n)])
-                i += 1
-            nodes[i][j] = (int(tokens[2][:-1]), int(tokens[3][:-1]))
-            j = (j + 1) % n
+            nodes.append((int(tokens[2][:-1]), int(tokens[3][:-1])))
 
-        count = 0
-        for i1 in range(n):
-            for j1 in range(n):
-                for i2 in range(n):
-                    for j2 in range(n):
-                        if nodes[i1][j1][0] > 0 and (i1 != i2 or j1 != j2)\
-                        and nodes[i1][j1][0] <= nodes[i2][j2][1]:
-                            count += 1
+        n = len(nodes)
 
-        return count
+        pairs = set()
+        for i in range(n):
+            for j in range(n):
+                if i != j and nodes[i][0] > 0  \
+                and nodes[i][0] <= nodes[j][1] \
+                and (j, i) not in pairs:
+                    pairs.add((i, j))
+
+        return len(pairs)
--- day-22/part-2/ayoub.py
+++ day-22/part-2/ayoub.py
@@ -1,12 +1,61 @@
 from submission import Submission
+from copy import deepcopy
 
 
 class AyoubSubmission(Submission):
+    states = {}
 
     def author(self):
         return 'Ayoub'
 
     def run(self, s):
-        s = s.rstrip()
+        s = s.rstrip().split('\n')[2:]
 
-        
+        class State(object):
+
+            @staticmethod
+            def hash(m, pos):
+                s = []
+                for i in range(len(m)):
+                    s.append(' '.join(map(str, m[i])))
+
+                return hash(' '.join(s) + ' ' +str(pos))
+
+            def __init__(self, m, pos):
+                self.m = deepcopy(m)
+                self.pos = pos
+                self._h = None
+
+            def __hash__(self):
+                if self._h is not None:
+                    return self._h
+                self._h = State.hash(self.m, self.pos)
+                return self._h
+
+            def __eq__(self, o):
+                return isinstance(o, State) and hash(o) == self.__hash__()
+
+            def next_states(self):
+
+
+                return []
+
+        def new_state(m, p):
+            h = State.hash(m, p)
+            if h in self.states:
+                return self.states[h]
+            return self.states[h] = State(m, p)
+
+        nodes = []
+        i = -1
+        for line in s:
+            tokens = line.split()
+            name = tokens[0].split('-')
+            if int(name[1][1:]) > i:
+                i += 1
+                nodes.append([])
+
+            nodes[i].append((int(tokens[2][:-1]), int(tokens[3][:-1])))
+
+        states = set()
+        states.add(State(nodes))
